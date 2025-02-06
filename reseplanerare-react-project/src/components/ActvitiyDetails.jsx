e93a2082f078cf1b69abbcbfcfe6a341ff9de119
gragas <180623g3h34htctrl@users.noreply.github.com>
2025-02-06 14:26:17 +0100

    add dynamic route activity details

---
+++ reseplanerare-react-project/src/components/ActvitiyDetails.jsx
@@ -0,0 +1,13 @@
+import Header from './Header';
+
+function ActvitiyDetails() {
+  return (
+    <>
+      <Header />
+      <h2>activity details:</h2>
+      <p>ye</p>
+    </>
+  );
+}
+
+export default ActvitiyDetails;
--- reseplanerare-react-project/src/components/Details.jsx
+++ reseplanerare-react-project/src/components/Details.jsx
@@ -1,11 +1,23 @@
+import { useSelector } from 'react-redux';
 import Header from './Header';
+import { Link } from 'react-router-dom';
 
 function Details() {
+  const travel = useSelector((state) => state.travel.activities);
+
   return (
     <>
       <Header />
       <main>
         <h2>Details</h2>
+        {travel.map((trav) => {
+          return (
+            <div className='activity-item-details-page'>
+              <p>{trav.activity}</p>
+              <Link to={`/details/${trav.id}`}>Läs mer</Link>
+            </div>
+          );
+        })}
       </main>
     </>
   );
