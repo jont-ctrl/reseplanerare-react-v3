651339855b79490f7a909fcd601efe718e3b4921
gragas <180623g3h34htctrl@users.noreply.github.com>
2025-02-07 09:46:40 +0100

    update footer

--- reseplanerare-react-project/src/components/Footer.jsx
+++ reseplanerare-react-project/src/components/Footer.jsx
@@ -1,15 +1,18 @@
-import { Link } from "react-router-dom";
-import RandomCat from "./RandomCat";
+import { Link } from 'react-router-dom';
+import RandomCat from './RandomCat';
 import './Css/Footer.css';
 
 export default function Footer() {
   return (
     <footer>
-      <div className="footer-content">
-        <div className="trip-planner-info">
+      <div className='footer-content'>
+        <div className='trip-planner-info'>
           <h2>Planera din resa med oss!</h2>
-          <p>Vår reseplanerare hjälper dig att skapa den perfekta resan. Fyll i dina preferenser och få en skräddarsydd reseplan.</p>
-          <Link to="/new" className="footer-link">
+          <p>
+            Vår reseplanerare hjälper dig att skapa den perfekta resan. Fyll i
+            dina preferenser och få en skräddarsydd reseplan.
+          </p>
+          <Link to='/new' className='footer-link'>
             Planera din resa här
           </Link>
         </div>
