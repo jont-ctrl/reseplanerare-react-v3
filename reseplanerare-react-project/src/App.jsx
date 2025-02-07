608594d218da054b22b3d27ebcb11de7e6ddbc1b
Sekaziga <145746970+Sekaziga@users.noreply.github.com>
2025-02-07 09:45:21 +0100

    updted app

--- reseplanerare-react-project/src/App.jsx
+++ reseplanerare-react-project/src/App.jsx
@@ -2,13 +2,14 @@ import { useState, Suspense, lazy } from 'react';
 import Header from './components/Header';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+
 // Lazy load all components
 const HomePage = lazy(() => import('./components/HomePage'));
 const ActivityForm = lazy(() => import('./components/ActivityForm'));
 const ActivityList = lazy(() => import('./components/ActivityList'));
 const Details = lazy(() => import('./components/Details'));
 const PageNotFound = lazy(() => import('./components/PageNotFound'));
-
+ 
 const router = createBrowserRouter([
   {
     path: '/',
