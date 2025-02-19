50333c7275f4589da1c3ef494ee9c07f1eb7877f
gragas <180623g3h34htctrl@users.noreply.github.com>
2025-02-19 15:03:43 +0100

    add emojis

--- reseplanerare-react-project/src/components/ActivityList.jsx
+++ reseplanerare-react-project/src/components/ActivityList.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeActivity, updateActivity} from '../redux/travelSlice';
-import './Css/ActivityList.css'
+import { removeActivity, updateActivity } from '../redux/travelSlice';
+import './Css/ActivityList.css';
 
 // Två props
 function ActivityList() {
@@ -13,7 +13,6 @@ function ActivityList() {
   // För redigering
   const [editingActivity, setEditingActivity] = useState(null);
   const [editedValues, setEditedValues] = useState({});
-
 
   function removeItem(id) {
     dispatch(removeActivity(id));
@@ -38,76 +37,84 @@ function ActivityList() {
 
   // Spara redigering
   function saveEditing() {
-    dispatch(updateActivity({ id: editingActivity, updatedActivity: editedValues }));
+    dispatch(
+      updateActivity({ id: editingActivity, updatedActivity: editedValues })
+    );
     setEditingActivity(null);
     setEditedValues({});
   }
 
-// Hantera ändringar i fälten under redigering
-function handleInputChange(e, field) {
-  setEditedValues((prevValues) => ({
-    ...prevValues,
-    [field]: e.target.value,
-  }));
-}
+  // Hantera ändringar i fälten under redigering
+  function handleInputChange(e, field) {
+    setEditedValues((prevValues) => ({
+      ...prevValues,
+      [field]: e.target.value,
+    }));
+  }
 
-
-
-return (
-  <div className="activityList-area">
-  <ul className="activity-list">
-    {travel.map((item) => (
-      <li key={item.id} className="activity-item">
-        {editingActivity === item.id ? (
-          // Formulär för att redigera
-          <div className="edit-container">
-            <input
-              type="text"
-              value={editedValues.activity}
-              onChange={(e) => handleInputChange(e, 'activity')}
-              className="edit-input"
-              placeholder="Aktivitet"
-            />
-            <input
-              type="text"
-              value={editedValues.place}
-              onChange={(e) => handleInputChange(e, 'place')}
-              className="edit-input"
-              placeholder="Plats"
-            />
-            <input
-              type="time"
-              value={editedValues.time}
-              onChange={(e) => handleInputChange(e, 'time')}
-              className="edit-input"
-            />
-            <input
-              type="date"
-              value={editedValues.date}
-              onChange={(e) => handleInputChange(e, 'date')}
-              className="edit-input"
-            />
-            <div className="button-container">
-              <button className="saveBtn" onClick={saveEditing}>Spara</button>
-              <button className="cancelBtn" onClick={cancelEditing}>Avbryt</button>
-            </div>
-          </div>
-        ) : (
-          // Visa information för en aktivitet
-          <>
-            <h3 className="activity-title">{item.activity}</h3>
-            <p className="place-text">{item.place}</p>
-            <p className="time-text">{item.time}</p>
-            <p className="date-text">{item.date}</p>
-            <button className="editBtn" onClick={() => startEditing(item)}>Redigera</button>
-            <button className="addBtn" onClick={() => removeItem(item.id)}>Ta bort</button>
-          </>
-        )}
-      </li>
-    ))}
-  </ul>
-</div>
-);
+  return (
+    <div className='activityList-area'>
+      <ul className='activity-list'>
+        {travel.map((item) => (
+          <li key={item.id} className='activity-item'>
+            {editingActivity === item.id ? (
+              // Formulär för att redigera
+              <div className='edit-container'>
+                <input
+                  type='text'
+                  value={editedValues.activity}
+                  onChange={(e) => handleInputChange(e, 'activity')}
+                  className='edit-input'
+                  placeholder='Aktivitet'
+                />
+                <input
+                  type='text'
+                  value={editedValues.place}
+                  onChange={(e) => handleInputChange(e, 'place')}
+                  className='edit-input'
+                  placeholder='Plats'
+                />
+                <input
+                  type='time'
+                  value={editedValues.time}
+                  onChange={(e) => handleInputChange(e, 'time')}
+                  className='edit-input'
+                />
+                <input
+                  type='date'
+                  value={editedValues.date}
+                  onChange={(e) => handleInputChange(e, 'date')}
+                  className='edit-input'
+                />
+                <div className='button-container'>
+                  <button className='saveBtn' onClick={saveEditing}>
+                    Spara
+                  </button>
+                  <button className='cancelBtn' onClick={cancelEditing}>
+                    Avbryt
+                  </button>
+                </div>
+              </div>
+            ) : (
+              // Visa information för en aktivitet
+              <>
+                <h3 className='activity-title'>{item.activity}</h3>
+                <p className='place-text'>🗺️ {item.place}</p>
+                <p className='time-text'>🕒 {item.time}</p>
+                <p className='date-text'>📅 {item.date}</p>
+                <button className='editBtn' onClick={() => startEditing(item)}>
+                  Redigera
+                </button>
+                <button className='addBtn' onClick={() => removeItem(item.id)}>
+                  Ta bort
+                </button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ActivityList;
